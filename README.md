4373c2475047ea33feb735a5a2ec4be905ec6f2c
Oliver Margetts <oliver.margetts@gmail.com>
2023-08-17 16:55:39 +0100

    perf: :construction: add first draft of benchmarks

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # relativedelta
 A small fast implementation of relativedeltas
+
+# benchmarks
+
+Shifting 5mn datetime objects gives us:
+
+| interpreter | relativedelta | python-dateutil | speedup |
+| ------ | ------ | ------ | ------ |
+| cpython 3.11 | 6.72s | 20.36s | 3.03x |
+| pypy 3.9 | 0.72s | 3.13s | 4.34x |
---
+++ benches/bench.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+import sys
+from datetime import datetime, timedelta
+from timeit import timeit
+
+from dateutil.relativedelta import relativedelta as du_delta
+
+from relativedelta import relativedelta
+
+klass = sys.argv[1]
+
+
+dates = [datetime(2000, 1, 1) + timedelta(days=n) for n in range(5_000)]
+
+
+def do_shifts():
+    if klass == "relativedelta":
+        for d in dates:
+            d + relativedelta(years=100)
+    if klass == "dateutil":
+        for d in dates:
+            d + du_delta(years=100)
+
+
+print(klass, timeit(do_shifts, number=1000))
